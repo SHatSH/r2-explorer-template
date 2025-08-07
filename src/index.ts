331ba11363ecfb5a63c465adb9803e0a54857266
Sh@SH <lishanghui8@gmail.com>
2025-08-08 00:10:01 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { R2Explorer } from "r2-explorer";
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: false,
 
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
+import { R2Explorer } from 'r2-explorer';
+
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'teste',
+    password: 'abc'
+  },{
+    username: 'anotherUser',
+    password: '123567'
+  }]
 });
